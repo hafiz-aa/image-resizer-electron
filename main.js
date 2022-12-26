e6584d666910317a715ab09ddae73561dbde775e
hafiz-aa <mhafiz.aa@gmail.com>
2022-12-26 09:57:28 +0700

    Check if dev env

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
 const path = require('path')
 const { app, BrowserWindow } = require('electron')
 
+const isDev = process.env.NODE_ENV !== 'production'
+const isMac = process.platform === 'darwin'
+
 function createMainWindow() {
 	const mainWindow = new BrowserWindow({
 		title: 'Image Resizer',
-		width: 500,
+		width: isDev ? 1000 : 500,
 		height: 600
 	})
+
+	if (isDev) {
+		mainWindow.webContents.openDevTools()
+	}
 
 	mainWindow.loadFile(path.join(__dirname, './renderer/index.html'))
 }
 
 app.whenReady().then(() => {
 	createMainWindow()
+})
+
+app.on('window-all-closed', () => {
+	if (!isMac) {
+		app.quit()
+	}
 })
