edc7d25a7307c1e9c3f4e2f07bea0da6026956f9
hafiz-aa <mhafiz.aa@gmail.com>
2022-12-26 11:47:21 +0700

    Add form upload image

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -1,24 +1,25 @@
-// Some JavaScript to load the image and show the form. There is no actual backend functionality. This is just the UI
-
 const form = document.querySelector('#img-form');
+const img = document.querySelector('#img');
+const outputPath = document.querySelector('#output-path');
+const filename = document.querySelector('#filename');
+const heightInput = document.querySelector('#height');
+const widthInput = document.querySelector('#width');
 
 function loadImage(e) {
-  const file = e.target.files[0];
+	const file = e.target.files[0]
 
-  if (!isFileImage(file)) {
-      alert('Please select an image file');
-        return;
-  }
+	if (!isFileImage(file)) {
+		console.log('Please select an image')
+		return
+	}
 
-  form.style.display = 'block';
-  document.querySelector(
-    '#filename'
-  ).innerHTML = file.name;
+	console.log('Success')
 }
 
+// Check if the file is image
 function isFileImage(file) {
-    const acceptedImageTypes = ['image/gif', 'image/jpeg', 'image/png'];
-    return file && acceptedImageTypes.includes(file['type'])
+	const acceptedImageTypes = ['image/gif', 'image/png', 'image/jpeg']
+	return file && acceptedImageTypes.includes(file['type'])
 }
 
-document.querySelector('#img').addEventListener('change', loadImage);
+img.addEventListener('change', loadImage)
